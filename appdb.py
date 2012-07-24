004b725077a757c645a64cbf7f7a84c5172ac042
Janne Paalijarvi <jpaalija@gmail.com>
2012-07-24 06:48:37 +0300

    Password reset stuff commits to database ok.

--- appdb.py
+++ appdb.py
@@ -97,7 +97,7 @@ class Auth(Base):
 				temp_tuple.Auth.expiration_time = datetime.utcnow() + timedelta(days=30)
 		for new_token_user in missing_token_users:
 			token = Auth() # This user needs new token
-			token.user_id = new_token_user
+			token.user_id = new_token_user.id
 			token.token_type = 'pw_hash'
 			token.token_content = urlsafe_b64encode(bytes(uuid.uuid4()))
 			token.expiration_time = datetime.utcnow() + timedelta(days=30)
