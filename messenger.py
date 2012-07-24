00af7d0b7110cf635cc45cc2ad1e6543307c825b
Janne Paalijarvi <jpaalija@gmail.com>
2012-07-24 23:46:37 +0300

    Added advice to ignore erroneous email password reset messages.

--- messenger.py
+++ messenger.py
@@ -15,7 +15,7 @@ def send_password_reset_emails(reset_list, reset_url_base): # reset_list contain
 		#print recipient.email + "  :" + token.token_content
 		msg = MIMEText("You have requested a password reset on PirateHR for " + recipient.legal_name + "." + 
 			       " You can reset your password by using this link: " + reset_url_base + token.token_content + "\n\n"+
-			       "If you think that this is an error, you can contact PirateHR at " + settings['email_reset_from'])
+			       "If you think that this is an error, you can safely ignore this message")
 		msg['Subject'] = "Password reset from PirateHR"
 		msg['From'] = "PirateHR <" + settings['email_reset_from'] + ">"
 		msg['To'] = recipient.email
